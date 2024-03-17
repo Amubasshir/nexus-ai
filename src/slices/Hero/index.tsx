300e2f292ef6d8c2dd0efcddb3d278296c32ad7e
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-03-17 11:24:24 +0600

    bounded added

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,30 @@
+import Bounded from "@/components/Bounded";
+import ButtonLink from "@/components/ButtonLink";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <ButtonLink field={slice.primary.button_link}>Link</ButtonLink>
+      <>{slice.primary.button_label}</>
+      <PrismicNextImage field={slice.primary.image} />
+    </Bounded>
+  );
+};
+
+export default Hero;
